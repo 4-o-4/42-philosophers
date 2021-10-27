2ba8a1a9674d20f9fd614c6b848481e0b504f2ae
4-o-4 <yuzhakov.lev@gmail.com>
2021-10-27 20:01:16 +0300

    init: philosophers.h

---
+++ includes/philosophers.h
@@ -0,0 +1,13 @@
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <ctype.h> // isdigit
+
+# define ERROR "\e[31mError:\e[0m"
+# define INT_LEN 10
+
+int *ft_parser(const int argc, char **argv);
+
+#endif
