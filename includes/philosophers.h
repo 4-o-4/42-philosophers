bb809f87e7a28a1e6689d960fa9802a261f594e6
4-o-4 <yuzhakov.lev@gmail.com>
2021-11-29 20:05:13 +0300

    refactor: change structure of the project

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -3,11 +3,16 @@
 
 # include <stdio.h>
 # include <stdlib.h>
+# include <sys/time.h>
+# include <pthread.h>
 # include <ctype.h> // isdigit
 
 # define ERROR "\e[31mError:\e[0m"
 # define INT_LEN 10
 
-int *ft_parser(const int argc, char **argv);
+int     *ft_parser(const int argc, char **argv);
+int     ft_error(char *str);
+
+long    ft_gettime(void);
 
 #endif
