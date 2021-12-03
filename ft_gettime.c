24c4133bf601f97121fcc7254ea98edfbc564030
4-o-4 <yuzhakov.lev@gmail.com>
2021-12-03 21:19:12 +0300

    feat: add ft_usleep

--- ft_gettime.c
+++ ft_gettime.c
@@ -6,3 +6,11 @@ long ft_gettime(void) {
     gettimeofday(&current_time, NULL);
     return current_time.tv_sec * 1000 + current_time.tv_usec * 1e-3;
 }
+
+void ft_usleep(long time) {
+    long i;
+
+    i = ft_gettime() + time;
+    while (i > ft_gettime())
+        usleep(100);
+}
