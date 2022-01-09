57f99bdfb1b68e15921bb082af7ba2ea512e7569
4-o-4 <yuzhakov.lev@gmail.com>
2022-01-09 10:17:12 +0300

    feat: add errArg function

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include "./includes/philosophers.h"
 
+static int errArg(char *str) {
+    printf(ERROR " %s\n", str);
+    return 1;
+}
+
 int main(int argc, char **argv) {
     int *value;
 
     if (argc < 5 || argc > 6)
-        return ft_error("...");
+        return errArg("argc");
     value = ft_parser(argc, argv);
     if (!value)
         return 1;
-    else if (!ft_ww(argc, value))
+    else if (!ft_init(argc, value))
         return 1;
     return 0;
 }
