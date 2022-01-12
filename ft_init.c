e35bfd671a25fe6ec4de213d5f11e57c31e45480
4-o-4 <yuzhakov.lev@gmail.com>
2022-01-12 18:23:23 +0300

    feat: implement thread launch

--- ft_init.c
+++ ft_init.c
@@ -1,25 +1,16 @@
 #include "./includes/philosophers.h"
 
-static void swapMutex(pthread_mutex_t **firstFork, pthread_mutex_t **secondFork) {
-    pthread_mutex_t *c = *firstFork;
-    *firstFork = *secondFork;
-    *secondFork = c;
-}
-
 static t_philo *setFork(int i, t_philo *philo, pthread_mutex_t *forkMutex) {
+    philo[i].firstFork = forkMutex + i;
+    philo[i].secondFork = forkMutex;
     while (i--) {
         philo[i].firstFork = forkMutex + i;
-        if (!i)
-            philo[i].secondFork = forkMutex + philo->value[0] - 1;
-        else
-            philo[i].secondFork = forkMutex + i - 1;
-        if (i % 2 != 0)
-            swapMutex(&(philo + i)->firstFork, &(philo + i)->secondFork);
+        philo[i].secondFork = forkMutex + i + 1;
     }
     return philo;
 }
 
-static t_philo *initPhilo(int *value, pthread_mutex_t *forkMutex) {
+static t_philo *initPhilo(int argc, int *value, pthread_mutex_t *forkMutex) {
     t_philo *philo;
     int i;
 
@@ -28,11 +19,48 @@ static t_philo *initPhilo(int *value, pthread_mutex_t *forkMutex) {
     if (!philo)
         return NULL;
     while (i--) {
-        philo[i].id = i;
+        philo[i].id = i + 1;
         philo[i].value = value;
+        philo[i].ut0 = ft_getTime();
+        philo[i].arg5 = NULL;
+        if (argc == ARG5)
+            philo[i].arg5 = value + 4;
         pthread_mutex_init(forkMutex + i, NULL);
     }
-    return setFork(value[0], philo, forkMutex);
+    if (argc == ARG5)
+        value[4] *= value[0];
+    return setFork(value[0] - 1, philo, forkMutex);
+}
+
+static void createPhilo(int *value, t_philo *philo, pthread_t *threads) {
+    int i;
+
+    i = 0;
+    while (i < value[0]) {
+        pthread_create(threads + i, NULL, ft_philo, (void *) (philo + i));
+        i += 2;
+        if (i >= value[0] && !(i % 2))
+            i = ft_oddCounter();
+    }
+}
+
+static int startPhilo(int *value, pthread_mutex_t *forkMutex, t_philo *philo) {
+    pthread_t *threads;
+    int i;
+
+    threads = (pthread_t *)malloc(sizeof(pthread_t) * (value[0] + 1));
+    if (!threads)
+        return 0;
+    createPhilo(value, philo, threads);
+    pthread_create(threads + value[0], NULL, ft_watcher, (void *) philo);
+    i = 0;
+    while (i < value[0])
+        pthread_join(threads[i++], NULL);
+    pthread_join(threads[value[0]], NULL);
+    while (i--)
+        pthread_mutex_destroy(forkMutex + i);
+    free(threads);
+    return 1;
 }
 
 int ft_init(int argc, int *value) {
@@ -41,10 +69,11 @@ int ft_init(int argc, int *value) {
 
     forkMutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * value[0]);
     if (!forkMutex)
-        return 0;
-    philo = initPhilo(value, forkMutex);
+        return ft_free(value, NULL, NULL);
+    philo = initPhilo(argc, value, forkMutex);
     if (!philo)
-        return 0;
-
+        return ft_free(value, forkMutex, NULL);
+    if (!startPhilo(value, forkMutex, philo))
+        return ft_free(value, forkMutex, philo);
     return 1;
 }
